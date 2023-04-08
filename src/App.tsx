963c87701d9786f828c9d056a9a2bde3af040297
Ssejjemba Dan <ssejjembadan@gmail.com>
2023-04-09 02:04:31 +0300

    Add basic react integrations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { Editor } from "@monaco-editor/react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Editor
+        height="100%"
+        defaultLanguage="sql"
+        theme="vs-dark"
+        onMount={(editor, monaco) => {
+          monaco.editor.defineTheme("dark", {
+            base: "vs-dark",
+            inherit: true,
+            colors: {
+              "editor.background": "#011627",
+              "editor.foreground": "#a7dbf7",
+              "editor.inactiveSelectionBackground": "#7e57c25a",
+              "editor.hoverHighlightBackground": "#0c4994",
+              "editor.lineHighlightBackground": "#0c499477",
+              "editor.selectionBackground": "#103362",
+              "editor.selectionHighlightBackground": "#103362",
+              "editor.findMatchHighlightBackground": "#103362",
+              "editor.rangeHighlightBackground": "#103362",
+              "editor.wordHighlightBackground": "#103362",
+              "editor.wordHighlightStrongBackground": "#103362",
+            },
+            rules: [],
+          });
+
+          monaco.editor.setTheme("dark");
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
